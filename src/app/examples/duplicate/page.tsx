ddfce6a584730e600e54778fadebbe5e3bab3bbe
HaoWang95 <alanwang0028@gmail.com>
2024-03-21 00:25:37 +1100

    update: code read

--- src/app/examples/duplicate/page.tsx
+++ src/app/examples/duplicate/page.tsx
@@ -1,12 +1,16 @@
+import CodeFileReader from "@/app/ui/examples/CodeContentReader";
 import { lusitana } from "@/app/ui/font";
 
 export default function DuplicateCodePage() {
   return (
-    <div>
+    <div className="w-full">
       <div>
         <h1 className={`${lusitana.className} text-xl md:text-2xl mb-4`}>
           Duplicate code checker
         </h1>
+      </div>
+      <div className="w-full">
+        <CodeFileReader />
       </div>
     </div>
   );
--- src/app/examples/page.tsx
+++ src/app/examples/page.tsx
@@ -1,21 +1,22 @@
 import { IoTerminalOutline } from "react-icons/io5";
 import ExampleCard from "../ui/examples/ExampleCard";
 import { lusitana } from "../ui/font";
+import clsx from "clsx";
 
 export default function ExamplesPage() {
   return (
-    <main>
-      <h1 className={`${lusitana.className} text-xl md:text-2xl mb-4`}>
+    <div className="w-full">
+      <h1 className={clsx(lusitana.className, "text-xl md:text-2xl mb-4")}>
         Examples
       </h1>
       <div className="w-full">
         <ExampleCard
           title="Code check"
-          content="Duplicate code checking for your code snippet"
+          content="Duplicate code checking for your code snippet."
           icon={<IoTerminalOutline className="h-6 w-6" />}
           linkTo="/examples/duplicate"
         />
       </div>
-    </main>
+    </div>
   );
 }
