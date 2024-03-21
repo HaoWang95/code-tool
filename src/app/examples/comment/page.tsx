86fd23c87a95a0cf441af183f4dd2cd0c35fa10b
HaoWang95 <alanwang0028@gmail.com>
2024-03-22 10:02:46 +1100

    update: comment feature init

--- src/app/examples/comment/page.tsx
+++ src/app/examples/comment/page.tsx
@@ -1,3 +1,9 @@
-export default function CommentPage(){
-    
+import { lusitana } from "@/app/ui/font";
+
+export default function CommentPage() {
+  return (
+    <div className="w-full text-xl md:text-2xl mb-4">
+      <h1 className={`${lusitana.className}`}>Comment/explain code</h1>
+    </div>
+  );
 }
--- src/app/examples/page.tsx
+++ src/app/examples/page.tsx
@@ -2,6 +2,7 @@ import { IoTerminalOutline } from "react-icons/io5";
 import ExampleCard from "../ui/examples/ExampleCard";
 import { lusitana } from "../ui/font";
 import clsx from "clsx";
+import { BiCommentEdit } from "react-icons/bi";
 
 export default async function ExamplesPage() {
   return (
@@ -9,12 +10,18 @@ export default async function ExamplesPage() {
       <h1 className={clsx(lusitana.className, "text-xl md:text-2xl mb-4")}>
         Examples
       </h1>
-      <div className="w-full">
+      <div className="w-full flex flex-row justify-evenly">
         <ExampleCard
           title="Code check"
           content="Duplicate code checking for your code snippet."
           icon={<IoTerminalOutline className="h-6 w-6" />}
           linkTo="/examples/duplicate"
+        />
+        <ExampleCard
+          title="Code comment"
+          content="Quick code analysis and explain for your code snippet"
+          icon={<BiCommentEdit className="h-6 w-6" />}
+          linkTo="/examples/comment"
         />
       </div>
     </div>
